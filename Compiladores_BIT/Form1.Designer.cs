b8581cbe5f22e2a3bb5713e0889224ba78beda04
Pablo <pabloboysn64@gmail.com>
2021-02-10 19:37:34 -0600

    cambiando nombre a boton prueba

--- Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Form1.Designer.cs
@@ -38,7 +38,7 @@ namespace Compiladores_BIT
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "pun";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
