51244e955dca1e667e15b948884efc660aaa93c6
Pablo <pabloboysn64@gmail.com>
2021-02-10 20:20:08 -0600

    sicronizando

--- Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Compiladores_BIT
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 115);
+            this.button1.Location = new System.Drawing.Point(224, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
