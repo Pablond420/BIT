910e983f4989f629af73f5517a566c146824631e
Pablo <pabloboysn64@gmail.com>
2021-02-10 20:31:05 -0600

    eliminando boton

--- Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
@@ -29,21 +29,11 @@ namespace Compiladores_BIT
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(52, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "nuevo Nombre";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -90,7 +80,6 @@ namespace Compiladores_BIT
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,8 +87,6 @@ namespace Compiladores_BIT
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
