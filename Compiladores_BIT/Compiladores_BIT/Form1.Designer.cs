1da2135d93b576ef50e21bd1a3aa80d5b73b135e
Pablo <pabloboysn64@gmail.com>
2021-02-16 01:20:52 -0600

    lectura de texto

--- Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
@@ -30,67 +30,56 @@ namespace Compiladores_BIT
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.text_abrir = new System.Windows.Forms.TextBox();
+            this.abrir_txt = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(614, 47);
+            this.button2.BackColor = System.Drawing.Color.Khaki;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 33);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(175, 34);
             this.button2.TabIndex = 1;
-            this.button2.Text = "pepe";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Abrir archivo";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // text_abrir
             // 
-            this.button3.Location = new System.Drawing.Point(614, 91);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "pepe";
-            this.button3.UseVisualStyleBackColor = true;
+            this.text_abrir.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_abrir.Location = new System.Drawing.Point(12, 73);
+            this.text_abrir.Multiline = true;
+            this.text_abrir.Name = "text_abrir";
+            this.text_abrir.Size = new System.Drawing.Size(580, 302);
+            this.text_abrir.TabIndex = 2;
             // 
-            // button4
+            // abrir_txt
             // 
-            this.button4.Location = new System.Drawing.Point(614, 138);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(614, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.abrir_txt.FileName = "openFileDialog1";
+            this.abrir_txt.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(604, 387);
+            this.Controls.Add(this.text_abrir);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text_abrir;
+        private System.Windows.Forms.OpenFileDialog abrir_txt;
     }
 }
 
--- Compiladores_BIT/Compiladores_BIT/Form1.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.cs
@@ -16,5 +16,23 @@ namespace Compiladores_BIT
         {
             InitializeComponent();
         }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            abrir_txt.InitialDirectory = "C:/";
+
+            if(abrir_txt.ShowDialog()==DialogResult.OK)
+            {
+                System.IO.StreamReader txt = new System.IO.StreamReader(abrir_txt.FileName, System.Text.Encoding.UTF8);
+                string texto = txt.ReadToEnd();
+                txt.Close();
+                text_abrir.Text = texto;
+            }
+        }
     }
 }
