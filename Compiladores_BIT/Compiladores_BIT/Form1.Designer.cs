21edfc9d03a6abb3f4e3a5f3c7ee9c420ce0ab59
Pablo <pabloboysn64@gmail.com>
2021-02-24 15:53:50 -0600

    Mejorando interfaz

--- Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
@@ -32,6 +32,15 @@ namespace Compiladores_BIT
             this.button2 = new System.Windows.Forms.Button();
             this.text_abrir = new System.Windows.Forms.TextBox();
             this.abrir_txt = new System.Windows.Forms.OpenFileDialog();
+            this.posfija_text = new System.Windows.Forms.TextBox();
+            this.btn_posfija = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button2
@@ -39,9 +48,9 @@ namespace Compiladores_BIT
             this.button2.BackColor = System.Drawing.Color.Khaki;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 33);
+            this.button2.Location = new System.Drawing.Point(178, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(175, 34);
+            this.button2.Size = new System.Drawing.Size(116, 27);
             this.button2.TabIndex = 1;
             this.button2.Text = "Abrir archivo";
             this.button2.UseVisualStyleBackColor = false;
@@ -49,11 +58,11 @@ namespace Compiladores_BIT
             // 
             // text_abrir
             // 
-            this.text_abrir.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text_abrir.Location = new System.Drawing.Point(12, 73);
+            this.text_abrir.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_abrir.Location = new System.Drawing.Point(25, 36);
             this.text_abrir.Multiline = true;
             this.text_abrir.Name = "text_abrir";
-            this.text_abrir.Size = new System.Drawing.Size(580, 302);
+            this.text_abrir.Size = new System.Drawing.Size(409, 34);
             this.text_abrir.TabIndex = 2;
             // 
             // abrir_txt
@@ -61,18 +70,96 @@ namespace Compiladores_BIT
             this.abrir_txt.FileName = "openFileDialog1";
             this.abrir_txt.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
+            // posfija_text
+            // 
+            this.posfija_text.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.posfija_text.Location = new System.Drawing.Point(25, 128);
+            this.posfija_text.Multiline = true;
+            this.posfija_text.Name = "posfija_text";
+            this.posfija_text.Size = new System.Drawing.Size(410, 34);
+            this.posfija_text.TabIndex = 3;
+            // 
+            // btn_posfija
+            // 
+            this.btn_posfija.BackColor = System.Drawing.Color.Khaki;
+            this.btn_posfija.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_posfija.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_posfija.Location = new System.Drawing.Point(455, 36);
+            this.btn_posfija.Name = "btn_posfija";
+            this.btn_posfija.Size = new System.Drawing.Size(175, 34);
+            this.btn_posfija.TabIndex = 4;
+            this.btn_posfija.Text = "Representacion posfija";
+            this.btn_posfija.UseVisualStyleBackColor = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(664, 327);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FloralWhite;
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.btn_posfija);
+            this.tabPage1.Controls.Add(this.text_abrir);
+            this.tabPage1.Controls.Add(this.posfija_text);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(656, 301);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Analizador lexico";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(656, 301);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Expresion regular:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Posfija:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(604, 387);
-            this.Controls.Add(this.text_abrir);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(666, 340);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Compiladores e interpretes";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -80,6 +167,13 @@ namespace Compiladores_BIT
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox text_abrir;
         private System.Windows.Forms.OpenFileDialog abrir_txt;
+        private System.Windows.Forms.TextBox posfija_text;
+        private System.Windows.Forms.Button btn_posfija;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
