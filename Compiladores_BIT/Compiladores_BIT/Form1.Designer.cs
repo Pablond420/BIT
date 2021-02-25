76dc2838e8502886307e12a832106c650ef35a60
Dennise Monserrath Ibarra Cuevas <53837664+denscuevs@users.noreply.github.com>
2021-02-25 10:33:38 -0600

    Agrega portada e integrantes

--- Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
+++ Compiladores_BIT/Compiladores_BIT/Form1.Designer.cs
@@ -29,47 +29,75 @@ namespace Compiladores_BIT
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.text_abrir = new System.Windows.Forms.TextBox();
             this.abrir_txt = new System.Windows.Forms.OpenFileDialog();
-            this.posfija_text = new System.Windows.Forms.TextBox();
-            this.btn_posfija = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.label2 = new System.Windows.Forms.Label();
+            this.posfija_text = new System.Windows.Forms.TextBox();
+            this.text_abrir = new System.Windows.Forms.TextBox();
+            this.btn_posfija = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
             this.regEx_explicita = new System.Windows.Forms.TextBox();
-            this.tabControl1.SuspendLayout();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Khaki;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(25, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 27);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Abrir archivo";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // text_abrir
-            // 
-            this.text_abrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text_abrir.Location = new System.Drawing.Point(25, 75);
-            this.text_abrir.Multiline = true;
-            this.text_abrir.Name = "text_abrir";
-            this.text_abrir.Size = new System.Drawing.Size(409, 34);
-            this.text_abrir.TabIndex = 2;
             // 
             // abrir_txt
             // 
             this.abrir_txt.FileName = "openFileDialog1";
             this.abrir_txt.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(519, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Compiladores e Intérpretes A - Equipo BIT";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(211, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Badillo Ortíz Pablo Angel";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(164, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(336, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ibarra Cuevas Dennise Monserrath";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FloralWhite;
+            this.tabPage1.Controls.Add(this.regEx_explicita);
+            this.tabPage1.Controls.Add(this.text_abrir);
+            this.tabPage1.Controls.Add(this.posfija_text);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.btn_posfija);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(656, 301);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Analizador lexico";
             // 
             // posfija_text
             // 
@@ -80,6 +108,15 @@ namespace Compiladores_BIT
             this.posfija_text.ReadOnly = true;
             this.posfija_text.Size = new System.Drawing.Size(410, 34);
             this.posfija_text.TabIndex = 3;
+            // 
+            // text_abrir
+            // 
+            this.text_abrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_abrir.Location = new System.Drawing.Point(25, 75);
+            this.text_abrir.Multiline = true;
+            this.text_abrir.Name = "text_abrir";
+            this.text_abrir.Size = new System.Drawing.Size(409, 34);
+            this.text_abrir.TabIndex = 2;
             // 
             // btn_posfija
             // 
@@ -94,32 +131,28 @@ namespace Compiladores_BIT
             this.btn_posfija.UseVisualStyleBackColor = false;
             this.btn_posfija.Click += new System.EventHandler(this.btn_posfija_Click);
             // 
-            // tabControl1
+            // button2
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(1, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(664, 327);
-            this.tabControl1.TabIndex = 5;
+            this.button2.BackColor = System.Drawing.Color.Khaki;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(25, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Abrir archivo";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // tabPage1
+            // label1
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.FloralWhite;
-            this.tabPage1.Controls.Add(this.regEx_explicita);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.button2);
-            this.tabPage1.Controls.Add(this.btn_posfija);
-            this.tabPage1.Controls.Add(this.text_abrir);
-            this.tabPage1.Controls.Add(this.posfija_text);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(656, 301);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Analizador lexico";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Expresión regular:";
             // 
             // label2
             // 
@@ -131,26 +164,6 @@ namespace Compiladores_BIT
             this.label2.TabIndex = 6;
             this.label2.Text = "Posfija:";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Expresion regular:";
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(656, 301);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // regEx_explicita
             // 
             this.regEx_explicita.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -161,34 +174,49 @@ namespace Compiladores_BIT
             this.regEx_explicita.Size = new System.Drawing.Size(409, 34);
             this.regEx_explicita.TabIndex = 7;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 91);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(664, 327);
+            this.tabControl1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(666, 340);
+            this.ClientSize = new System.Drawing.Size(666, 421);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Compiladores e interpretes";
-            this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox text_abrir;
         private System.Windows.Forms.OpenFileDialog abrir_txt;
-        private System.Windows.Forms.TextBox posfija_text;
-        private System.Windows.Forms.Button btn_posfija;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox regEx_explicita;
+        private System.Windows.Forms.TextBox text_abrir;
+        private System.Windows.Forms.TextBox posfija_text;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TextBox regEx_explicita;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_posfija;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
